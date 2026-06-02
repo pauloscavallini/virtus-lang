5196470e2eb894592d802f634c96304dd634b868
pauloscavallini <paulo.cavallini08@gmail.com>
2026-06-02 11:13:17 -0300

    first commit

---
+++ src/test/java/com/application/loxlang/LoxlangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.loxlang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoxlangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
